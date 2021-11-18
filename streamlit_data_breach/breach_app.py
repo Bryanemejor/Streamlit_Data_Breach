58c1150ee844872a96167fe85a3bc8b15f15c839
Bryanemejor <93387768+Bryanemejor@users.noreply.github.com>
2021-11-18 12:20:01 +0100

    Add files via upload

---
+++ streamlit_data_breach/breach_app.py
@@ -0,0 +1,25 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+import numpy as np
+import joblib
+
+
+# In[2]:
+
+
+st.markdown('## Data Breach Incident')
+affected_state = st.text_input('Enter State')
+method_breach = st.text_input('Enter Method of Breach')
+
+
+#Predict button
+if st.button('Click to Predict'):
+    model = joblib.load('/users/guembeblessing/desktop/data_breach/security_incident.pkl')
+    
+    st.markdown(f'### Prediction is {model.predict([[affected_state, method_breach]])[0]}')
+
