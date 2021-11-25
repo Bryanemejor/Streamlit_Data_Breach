fcf704869fdebd882fcb08818c7d4ac6d31f2b44
Bryanemejor <93387768+Bryanemejor@users.noreply.github.com>
2021-11-25 12:23:44 +0100

    Update breach_app.py

--- streamlit_data_breach/breach_app.py
+++ streamlit_data_breach/breach_app.py
@@ -16,10 +16,16 @@ st.markdown('## Data Breach Incident')
 affected_state = st.text_input('Enter State')
 method_breach = st.text_input('Enter Method of Breach')
 
-
+model = joblib.load('streamlit_data_breach/security_incident.pkl')
 #Predict button
-if st.button('Click to Predict'):
-    model = joblib.load('streamlit_data_breach/security_incident.pkl')
-    
-    st.markdown(f'### Prediction is {model.predict([[affected_state, method_breach]])[0]}')
 
+inputs = [[affected_state]]
+
+  if st.button('Predict'):
+    result = model.predict(inputs)
+    updated_res = result.flatten().astype(float)
+    st.success('The Probability of getting admission is {}'.format(updated_res))
+
+
+if __name__ =='__main__':
+  main()
