2133b164a02f19356576ed1c1a8cf3868b77cfef
Bryanemejor <93387768+Bryanemejor@users.noreply.github.com>
2021-11-28 06:50:25 +0100

    Update breach_app.py

--- streamlit_data_breach/breach_app.py
+++ streamlit_data_breach/breach_app.py
@@ -24,7 +24,7 @@ inputs = [[affected_state]]
 if st.button('Predict'):
    result = model.predict(inputs)
    #updated_res = result.flatten().astype(float)
-   #st.success('The Probability data breach in this city is {}'.format(updated_res))
-   st.success('The Probability of ', method_breach , 'in this city {}' .format(result))
+   st.success('The Probability data breach in this city is {}'.format(result))
+   #st.success('The Probability of ', method_breach , 'in this city {}' .format(result))
 
 
